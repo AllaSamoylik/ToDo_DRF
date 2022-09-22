beeb2136c7b7840bc1997a166692bf9707e0d07b
AllaSamoylik <zzajka@yandex.ru>
2022-09-22 14:23:41 +0300

    add axios & cors

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
-import logo from './logo.svg';
 import './App.css';
 import React from "react";
 import UserList from "./components/User";
+import axios from "axios";
 
 class App extends React.Component {
     constructor(props) {
@@ -12,27 +12,27 @@ class App extends React.Component {
     }
 
     componentDidMount() {
-        const users = [
-            {
-                'username': 'Фёдор',
-                'first_name': 'Фёдор',
-                'last_name': 'Достоевский',
-                'email': 1821
-            },
-            {
-                'username': 'Фёдор',
-                'first_name': 'Александр',
-                'last_name': 'Грин',
-                'email': 1880
-            },
-        ]
-        this.setState(
-            {
+        // const users = [
+        //     {
+        //         'username': 'Фёдор',
+        //         'first_name': 'Фёдор',
+        //         'last_name': 'Достоевский',
+        //         'email': 1821
+        //     },
+        //     {
+        //         'username': 'Фёдор',
+        //         'first_name': 'Александр',
+        //         'last_name': 'Грин',
+        //         'email': 1880
+        //     },
+        // ]
+        axios.get('http://127.0.0.1:8000/api/users/').then(response => {
+            const users = response.data
+            this.setState({
                 'users': users
-            }
-        )
+            })
+        }).catch(error => console.log(error))
     }
-
 
     render() {
         return (
--- frontend/src/components/User.js
+++ frontend/src/components/User.js
@@ -3,18 +3,10 @@ import React from 'react'
 const UserItem = ({user}) => {
     return (
         <tr>
-            <td>
-                {user.username}
-            </td>
-            <td>
-                {user.first_name}
-            </td>
-            <td>
-                {user.last_name}
-            </td>
-            <td>
-                {user.email}
-            </td>
+            <td>{user.username}</td>
+            <td>{user.first_name}</td>
+            <td>{user.last_name}</td>
+            <td>{user.email}</td>
         </tr>
     )
 }
@@ -23,19 +15,17 @@ const UserItem = ({user}) => {
 const UserList = ({users}) => {
     return (
         <table>
-            <th>
-                Username
-            </th>
-            <th>
-                First Name
-            </th>
-            <th>
-                Last Name
-            </th>
-            <th>
-                Email
-            </th>
-            {users.map((user_) => <UserItem user={user_}/>)}
+            <thead>
+            <tr>
+                <th>Username</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Email</th>
+            </tr>
+            </thead>
+            <tbody>
+                {users.map((user_, index) => <UserItem key={index} user={user_}/>)}
+            </tbody>
         </table>
     )
 }
