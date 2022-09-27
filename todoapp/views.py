7684f6a9109c1c575dc9825bfdd22765aa4f3186
AllaSamoylik <zzajka@yandex.ru>
2022-09-27 23:37:41 +0300

    add django filter & method override (model:todo)

--- todoapp/views.py
+++ todoapp/views.py
@@ -1,4 +1,7 @@
+from rest_framework import status
+from rest_framework.mixins import DestroyModelMixin
 from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from .models import Project, ToDo
 from .serializers import ProjectModelSerializer, ToDoModelSerializer
@@ -31,3 +34,10 @@ class ToDoModelViewSet(ModelViewSet):
     queryset = ToDo.objects.all()
     serializer_class = ToDoModelSerializer
     pagination_class = ToDoLimitOffsetPagination
+    filterset_fields = ['project', 'user']
+
+    def destroy(self, request, *args, **kwargs):
+        todo = self.get_object()
+        todo.is_active = False
+        todo.save()
+        return Response(status=status.HTTP_204_NO_CONTENT)
