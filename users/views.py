b847c30e490cfd8301a66847177bf56177853e60
AllaSamoylik <zzajka@yandex.ru>
2022-09-27 21:52:47 +0300

    add custom viewset (model:customuser)

--- users/views.py
+++ users/views.py
@@ -1,8 +1,9 @@
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import GenericViewSet
+from rest_framework.mixins import ListModelMixin, RetrieveModelMixin, UpdateModelMixin
 from .models import CustomUser
 from .serializers import CustomUserModelSerializer
 
 
-class CustomUserModelViewSet(ModelViewSet):
+class CustomUserCustomViewSet(GenericViewSet, ListModelMixin, RetrieveModelMixin, UpdateModelMixin):
     queryset = CustomUser.objects.all()
     serializer_class = CustomUserModelSerializer
