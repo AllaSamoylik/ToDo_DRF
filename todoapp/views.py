71c68156f8d46e7722bdded44e597b275e5095b5
AllaSamoylik <zzajka@yandex.ru>
2022-09-27 16:22:55 +0300

    add pagination

--- todoapp/views.py
+++ todoapp/views.py
@@ -1,13 +1,24 @@
+from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.viewsets import ModelViewSet
 from .models import Project, ToDo
 from .serializers import ProjectModelSerializer, ToDoModelSerializer
 
 
+class ProjectLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 10
+
+
+class ToDoLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 20
+
+
 class ProjectModelViewSet(ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectModelSerializer
+    pagination_class = ProjectLimitOffsetPagination
 
 
 class ToDoModelViewSet(ModelViewSet):
     queryset = ToDo.objects.all()
     serializer_class = ToDoModelSerializer
+    pagination_class = ToDoLimitOffsetPagination
