4e415a40da399c33c221199e518dcf20338bd1d6
AllaSamoylik <zzajka@yandex.ru>
2022-09-27 22:57:34 +0300

    add part filtering (model:project)

--- todo/settings.py
+++ todo/settings.py
@@ -126,5 +126,6 @@ REST_FRAMEWORK = {
         'rest_framework.renderers.BrowsableAPIRenderer',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 100
+    'PAGE_SIZE': 100,
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
 }
--- todoapp/views.py
+++ todoapp/views.py
@@ -2,6 +2,7 @@ from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.viewsets import ModelViewSet
 from .models import Project, ToDo
 from .serializers import ProjectModelSerializer, ToDoModelSerializer
+from .filters import ProjectFilter
 
 
 class ProjectLimitOffsetPagination(LimitOffsetPagination):
@@ -16,6 +17,14 @@ class ProjectModelViewSet(ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectModelSerializer
     pagination_class = ProjectLimitOffsetPagination
+    filterset_class = ProjectFilter
+    # # Фильтрация через параметры, напр. http://127.0.0.1:8000/api/projects/?title=cha
+    # def get_queryset(self):
+    #     title = self.request.query_params.get('title', '')
+    #     projects = Project.objects.all()
+    #     if title:
+    #         projects = projects.filter(title__contains=title)
+    #     return projects
 
 
 class ToDoModelViewSet(ModelViewSet):
