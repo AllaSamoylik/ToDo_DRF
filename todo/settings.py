97e7b6e473ac751aacc01d0986f52ac8882c3300
AllaSamoylik <zzajka@yandex.ru>
2022-10-05 14:35:30 +0300

    add basic auth

--- todo/settings.py
+++ todo/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'users',
     'todoapp',
     'corsheaders',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -129,7 +130,12 @@ REST_FRAMEWORK = {
     # 'PAGE_SIZE': 100,
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+        'rest_framework.permissions.IsAuthenticated'
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
     ],
 
 }
