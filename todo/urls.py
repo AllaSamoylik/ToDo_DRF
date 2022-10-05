b30248f79fb38fefda0ca2aebf54e8dea845eeec
AllaSamoylik <zzajka@yandex.ru>
2022-10-05 14:51:26 +0300

    add token auth

--- todo/urls.py
+++ todo/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.authtoken.views import obtain_auth_token
+
 from rest_framework.routers import DefaultRouter
 
 from users.views import CustomUserCustomViewSet
@@ -13,5 +15,6 @@ router.register('todos', ToDoModelViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls'))
+    path('api-auth/', include('rest_framework.urls')),
+    path('api-token-auth/', obtain_auth_token),
 ]
