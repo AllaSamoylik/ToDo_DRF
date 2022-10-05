32c65a4059b1211c26e62573996e29c752e2193f
AllaSamoylik <zzajka@yandex.ru>
2022-10-05 13:15:00 +0300

    permissions setup

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
-from users import models
+from users.models import CustomUser
+from django.contrib.auth.admin import UserAdmin
 
 
-@admin.register(models.CustomUser)
-class Admin(admin.ModelAdmin):
-    pass
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('email',)}),
+    )
+    list_display = ('id', 'username', 'custom_group', 'email',)
+    list_display_links = ('username',)
+    ordering = ['id']
+
+    def custom_group(self, obj):
+        return ','.join([g.name for g in obj.groups.all()]) if obj.groups.count() else ''
