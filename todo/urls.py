fe23d40a593f012d72808842e718cc83f090858f
AllaSamoylik <zzajka@yandex.ru>
2022-10-07 15:24:28 +0300

    Lesson 6 (#6)
    * Lesson 6 ready

--- todo/urls.py
+++ todo/urls.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
+
 from rest_framework.routers import DefaultRouter
 
 from users.views import CustomUserCustomViewSet
@@ -13,5 +16,9 @@ router.register('todos', ToDoModelViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls'))
+    path('api-auth/', include('rest_framework.urls')),
+    path('api-token-auth/', obtain_auth_token),
+    path('api-jwt-token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api-jwt-token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api-jwt-token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
