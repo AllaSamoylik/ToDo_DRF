fb4574358441c62f73626b5552ffb7695e602773
AllaSamoylik <zzajka@yandex.ru>
2022-10-14 14:24:48 +0300

    added acceptheader _versioning

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,10 @@
-from django.test import TestCase
+import requests
 
-# Create your tests here.
+# ----------------------
+# Для AcceptHeaderVersioning:
+response = requests.get('http://127.0.0.1:8000/api/users/')
+print(response.json())
+
+response = requests.get('http://127.0.0.1:8000/api/users/', headers={'Accept': 'application/json; version=0.2'})
+print(response.json())
+# ----------------------
