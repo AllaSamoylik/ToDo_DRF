65e99e18551cdb7baec549418d1e0066c6b79691
AllaSamoylik <zzajka@yandex.ru>
2022-10-22 13:23:08 +0300

    added search func

--- frontend/src/components/Project.js
+++ frontend/src/components/Project.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, {useState} from "react";
 import {Link} from "react-router-dom";
+
 
 const ProjectItem = ({project, all_users, delete_project}) => {
     return (
@@ -10,8 +11,8 @@ const ProjectItem = ({project, all_users, delete_project}) => {
             <td>{project.link}</td>
             <td>
                 {project.users.map((user_id) => {
-                let user = all_users.find(user => user.id === user_id)
-                return user.username + ' '
+                    let user = all_users.find(user => user.id === user_id)
+                    return user.username + ' '
                 })}
             </td>
             <td>
@@ -24,25 +25,38 @@ const ProjectItem = ({project, all_users, delete_project}) => {
 }
 
 const ProjectList = ({projects, users, delete_project}) => {
+
+    const [value, setValue] = useState("")
+    const filteredProjects = projects.filter((project) => {
+        return project.title.toLowerCase().includes(value.toLowerCase())
+    })
+
     return (
         <div>
+            <input style={{marginBottom: '20px'}}
+                type="text"
+                placeholder="Search"
+                onChange={(event) => setValue(event.target.value)}
+            />
             <table style={{margin: '0 auto', width: '70%', tableLayout: 'fixed'}}>
-            <thead>
-            <tr>
-                <th>Project name</th>
-                <th>Link</th>
-                <th>Authors</th>
-                <th></th>
-            </tr>
-            </thead>
-            <tbody>
-            {projects.map((project_, index) => <ProjectItem key={index} project={project_}
-                                                            delete_project={delete_project}
-                                                            all_users={users}/>)}
-            </tbody>
-        </table><br></br>
-        <Link to='/projects/update'>Update</Link><br></br>
-        <Link to='/projects/create'>Create</Link>
+                <thead>
+                <tr>
+                    <th>Project name</th>
+                    <th>Link</th>
+                    <th>Authors</th>
+                    <th></th>
+                </tr>
+                </thead>
+                <tbody>
+                {filteredProjects.map((project_, index) => <ProjectItem key={index}
+                                                                project={project_}
+                                                                delete_project={delete_project}
+                                                                all_users={users}/>)}
+                </tbody>
+            </table>
+            <br></br>
+            <Link to='/projects/update'>Update</Link><br></br>
+            <Link to='/projects/create'>Create</Link>
         </div>
     )
 }
